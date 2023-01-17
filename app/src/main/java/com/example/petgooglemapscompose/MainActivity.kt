5c19b8dfd43f4930845aa6dc5e3780e2b23bb39f
toha <toha1988cool@gmail.com>
2023-01-17 16:52:08 +0300

    add geolocation

--- app/src/main/java/com/example/petgooglemapscompose/MainActivity.kt
+++ app/src/main/java/com/example/petgooglemapscompose/MainActivity.kt
@@ -3,18 +3,26 @@ package com.example.petgooglemapscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.petgooglemapscompose.ui.theme.PetGoogleMapsComposeTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.*
+
+/*
+Создать приложение с основным экраном в виде карты и следующими требованиями:
+● Приложение должно находить устройство по геолокации.
+● На карте можно ставить маркеры.
+● На отдельном экране можно посмотреть и отредактировать список маркеров, добавить к маркеру название и аннотацию.
+● Выбор архитектуры и библиотек остается за вами.
+*/
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,17 +30,48 @@ class MainActivity : ComponentActivity() {
         setContent {
             PetGoogleMapsComposeTheme {
 
-                val singapore = LatLng(1.35, 103.87)
-                val cameraPositionState = rememberCameraPositionState {
-                    position = CameraPosition.fromLatLngZoom(singapore, 10f)
-                }
 
-                GoogleMap(
-                    modifier = Modifier.fillMaxSize(),
-                    cameraPositionState = cameraPositionState
-                )
+                val singapore = LatLng(1.35, 103.87)
+                 val cameraPositionState = rememberCameraPositionState ()
+               /* val cameraPositionState = rememberCameraPositionState {
+                    position = CameraPosition.fromLatLngZoom(singapore, 10f)
+                }*/
+                var uiSettings by remember { mutableStateOf(MapUiSettings()) }
+                var properties by remember {
+                    mutableStateOf(MapProperties(mapType = MapType.HYBRID))
+                }
+                Box(Modifier.fillMaxSize()) {
+                    GoogleMap(
+                        modifier = Modifier.matchParentSize(),
+                       // properties = properties,
+                        properties = MapProperties(isMyLocationEnabled = true),
+                        uiSettings = uiSettings,
+                        cameraPositionState = cameraPositionState
+                    )
+
+
+                    Column() {
+                        Switch(
+                            checked = uiSettings.zoomControlsEnabled,
+                            onCheckedChange = {
+                                uiSettings = uiSettings.copy(zoomControlsEnabled = it)
+                            }
+                        )
+
+                        Button(onClick = {
+
+                        }) {
+
+                        }
+                    }
+
+
+
+                }
 
             }
         }
     }
+
+
 }
